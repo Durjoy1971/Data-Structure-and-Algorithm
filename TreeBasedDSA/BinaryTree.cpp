42a3ce9ad4f502678c9c76094de725b5eeac0319
Durjoy1971 <durjoybarua1971@gmail.com>
2024-04-21 11:58:56 +0600

    BinaryTree Done

---
+++ TreeBasedDSA/BinaryTree.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+
+using namespace std;
+
+struct Node
+{
+    int data;
+    Node *left, *right;
+};
+
+Node *newNode(int val)
+{
+    Node *tmp = (Node *)malloc(sizeof(Node));
+
+    tmp->data = val;
+    tmp->left = tmp->right = NULL;
+
+    return tmp;
+}
+
+// Traverse Preorder
+void traversePreOrder(Node *temp)
+{
+    if (temp != NULL)
+    {
+        cout << " " << temp->data;
+        traversePreOrder(temp->left);
+        traversePreOrder(temp->right);
+    }
+}
+
+// Traverse Inorder
+void traverseInOrder(Node *temp)
+{
+    if (temp != NULL)
+    {
+        traverseInOrder(temp->left);
+        cout << " " << temp->data;
+        traverseInOrder(temp->right);
+    }
+}
+
+// Traverse Postorder
+void traversePostOrder(Node *temp)
+{
+    if (temp != NULL)
+    {
+        traversePostOrder(temp->left);
+        traversePostOrder(temp->right);
+        cout << " " << temp->data;
+    }
+}
+
+int main()
+{
+    Node *root = newNode(1);
+    root->left = newNode(2);
+    root->right = newNode(3);
+    root->left->left = newNode(4);
+
+    cout << "preorder traversal: ";
+    traversePreOrder(root);
+    cout << "\nInorder traversal: ";
+    traverseInOrder(root);
+    cout << "\nPostorder traversal: ";
+    traversePostOrder(root);
+}
